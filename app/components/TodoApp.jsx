2edfc39c3ed62202e94d845b51d07434cb3cdafb
samgrimm <samantha.grimm@gmail.com>
2017-03-23 07:13:47 -0500

    complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var uuid = require('node-uuid');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -11,23 +12,31 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog'
         }, {
-          id: 2,
+          id: uuid(),
           text: 'Clean the kitchen'
         }, {
-          id: 3,
+          id: uuid(),
           text: 'Play with Tito'
         }, {
-          id: 4,
+          id: uuid(),
           text: 'Go swimming'
         }
       ]
     }
   },
   handleAddTodo: function (text) {
-    alert('new todo: ' + text);
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    })
   },
   handleSearch: function (showCompleted, searchText) {
     this.setState({
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,14 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  it('should add todo to the to-do state on the handleAddTodo', ()=> {
+    var todoText = 'testing 1 2 3';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
