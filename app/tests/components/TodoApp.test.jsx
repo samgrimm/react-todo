682afe92715fdd164bb10fabe7ccd6790c1aba2e
samgrimm <samantha.grimm@gmail.com>
2017-03-23 20:33:57 -0500

    added the created at and completed at attrs and how to set them

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+var moment = require('moment');
 
 var TodoApp = require('TodoApp');
 
@@ -19,13 +20,16 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos[0].createdAt).toBe(moment().unix());
   });
 
   it('should toggle completed value when handleToggle called',() => {
     var todoData = {
       id: 11,
       text: 'Test features',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
     todoApp.setState({todos: [todoData]});
@@ -33,5 +37,21 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(11);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBe(moment().unix());
+  });
+  it('should remove completedAt stamp when handleToggle set to false',() => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({todos: [todoData]});
+    todoApp.handleToggle(11);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toBe(undefined);
   });
 });
